12ecc25679ead9435848072393c97621adba5266
zidanebachtiar13 <zidanebachtiar13@gmail.com>
2022-06-20 12:00:43 +0700

    update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject simple-web "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple note web"
+  :url "https://simple-note-web.herokuapp.com/"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
