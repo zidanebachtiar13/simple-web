e629e9031b081c13f9f4c3ce839aa51ede3f1272
zidanebachtiar13 <zidanebachtiar13@gmail.com>
2022-06-22 18:01:18 +0700

    update for css

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [hiccup "2.0.0-alpha2"]]
+                 [hiccup "2.0.0-alpha2"]
+                 [garden "1.3.10"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler simple-web.handler/app}
   :profiles
--- src/simple_web/pages.clj
+++ src/simple_web/pages.clj
@@ -1,46 +1,39 @@
 (ns simple-web.pages
   (:require [hiccup.page :refer [html5]]
             [hiccup.form :as form]
+            [garden.core :refer [css]]
             [ring.util.anti-forgery :refer (anti-forgery-field)]))
+
+(def style
+  (css [:label {:margin-left "40px"}] 
+       ["input[type=text]" 
+        {:width "20%"
+         :margin "10px 10px 20px 40px"
+         :padding "10px"
+         :display "inline-block"
+         :border "1px solid #ccc"
+         :border-radius "4px"
+         :box-sizing "border-box"}]
+       ["input[type=submit]" 
+        {:width "90px"
+         :background-color "#e7e7e7e7"
+         :padding "7px"
+         :border "2px solid #e7e7e7"
+         :border-radius "4px"
+         :cursor "pointer"}
+        [:&:hover {:background-color "#D3D3D3"
+                   :border "2px solid #D3D3D3"}]]
+       [:p
+        {:border "1px solid black"
+         :border-radius "5px"
+         :padding "15px"
+         :text-align "center"}]))
 
 (defn base-page [& body]
   (html5
     [:head 
      [:title "Simple Web"]
-     [:style "label {
-                margin-left: 40px;
-              }
-
-              input[type=text] {
-                width: 20%;
-                margin: 10px 10px 20px 40px;
-                padding: 10px;
-                display: inline-block;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                box-sizing: border-box;
-              }
-             
-              input[type=submit] {
-                width:90px;
-                background-color: #e7e7e7e7;
-                padding: 7px;
-                border: 2px solid #e7e7e7;
-                border-radius: 4px;
-                cursor: pointer;
-              }
-              
-              input[type=submit]:hover {
-                background-color: #D3D3D3;
-                border: 2px solid #D3D3D3;
-              }
-             
-              p {
-                border: 1px solid black;
-                border-radius: 5px;
-                padding: 15px;
-                text-align: center;
-              }"]]
+     [:style style]]
     [:body
      (form/form-to
        [:post "/"]
